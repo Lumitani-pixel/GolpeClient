b4767c59255b2d13d82793bb31089c981a8ba92d
NormalV <moritz@fackler.cloud>
2025-11-03 21:41:47 +0100

    Initial commit

---
+++ src/main/java/net/normalv/golpeclient/Launcher.java
@@ -0,0 +1,9 @@
+package net.normalv.golpeclient;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(MainApplication.class, args);
+    }
+}
